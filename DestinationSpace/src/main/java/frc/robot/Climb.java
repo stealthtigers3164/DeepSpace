24ee99f507029b2095cc27e41624075ef0eb085e
maz <mazhurtado@gmail.com>
2019-03-02 10:12:29 -0500

    Completed the class for climbing

--- DestinationSpace/src/main/java/frc/robot/Climb.java
+++ DestinationSpace/src/main/java/frc/robot/Climb.java
@@ -7,9 +7,33 @@ public class Climb {
     private DoubleSolenoid frontPiston;
     private Solenoid backPiston;
 
-    public Climb() {
-        frontPiston = new DoubleSolenoid(0, 0, 0); //accepts module number, forward port on PCM, reverse port on PCM
-        backPiston = new Solenoid(0, 0); //accepts module number, port on PCM
+    public Climb(int f_moduleNum, int f_forward, int f_reverse, int b_moduleNum, int b_forward) {
+        frontPiston = new DoubleSolenoid(f_moduleNum, f_forward, f_reverse); //accepts module number, forward port on PCM, reverse port on PCM
+        backPiston = new Solenoid(b_moduleNum, b_forward); //accepts module number, port on PCM
+    }
+
+    public void update(Gamepad gamepad) {
+        boolean hasBeenUsed = false;
+        boolean d_forward = gamepad.trigger.getLeftPressed(true);
+        boolean d_reverse = gamepad.trigger.getRightPressed(true);
+        boolean s_out = gamepad.buttons.BUTTON_LB.isOn();
+        if (d_forward && hasBeenUsed) {
+            frontPiston.set(Value.kForward);
+            hasBeenUsed = true;
+        }
+        if (d_reverse && !(hasBeenUsed)) {
+            frontPiston.set(Value.kReverse);
+            hasBeenUsed = true;
+        }
+        if (s_out && !(hasBeenUsed)) {
+            backPiston.set(true);
+            hasBeenUsed = true;
+        }
+        else {
+            frontPiston.set(Value.kOff);
+            backPiston.set(false);
+            hasBeenUsed = false;
+        }
     }
 
 //OKAAAAY DO WE EVEN HAVE GAMEPAD SPACE ANYMORE :~)
